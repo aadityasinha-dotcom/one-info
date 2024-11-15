c589e729f72e2b20801ea57c5b74f035188e536a
aadityasinha-dotcom <aadityasinha009@gmail.com>
2024-11-15 23:31:15 +0530

    added image

--- app/index.tsx
+++ app/index.tsx
@@ -15,11 +15,11 @@ export default function App() {
     <SafeAreaView className='bg-primary h-full'>
       <ScrollView contentContainerStyle={{ height: '100%' }}>
         <View className='w-full justify-center items-center min-h-[85vh] px-4'>
-          {/* <Image */}
-          {/*   source={images.logo} */}
-          {/*   className='w-[130px] h-[84px]' */}
-          {/*   resizeMode='contain' */}
-          {/* /> */}
+          <Image
+            source={images.logo}
+            className='w-[130px] h-[84px]'
+            resizeMode='contain'
+          />
           <Image 
             source={images.cards}
             className='max-w-[280px] w-full h-[200px]'
